4a9449c52eb96dcee29b466141fd53698aa10e35
setyapanggaraka <setyarakap02@gmail.com>
2022-10-14 01:32:59 +0700

    tambah update

--- router/router.js
+++ router/router.js
@@ -8,8 +8,9 @@ router.get('/course/:courseId', Controller.selectCourse)
 router.post('/course/:courseId', Controller.buyCourse)
 router.get('/create_course', Controller.addCourse)
 router.post('/create_course', Controller.createCourse)
-router.get('/create_course', Controller.updateCourse)
-router.post('/create_course', Controller.createUpdateCourse)
+router.get('/course/:courseId/edit', Controller.updateCourse)
+router.post('/course/:courseId/edit', Controller.createUpdateCourse)
 router.get('/course/:courseId/delete', Controller.deleteCourse)
+// router.get('/course/upload', Controller.uploadCourse)
 
 module.exports = router
